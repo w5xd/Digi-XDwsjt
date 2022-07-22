fc6f776f24242b6593bf4e86c1fd7cd3a6081fc9
Wayne, W5XD <w5xd@writelog.com>
2022-07-22 21:44:36 +0000

    checkpoint

--- XDrcv/ft8_decode.f90
+++ XDrcv/ft8_decode.f90
@@ -5,159 +5,162 @@
 ! 41, 47 and 50 symbols are available and not at any other number of symbols of data available.
 ! it does some optimization--the 47 and 50 runs use fortran static data saved from prior runs.
 ! But maybe not everyone wants to run the FT8 decoder that way...
-! 
-!This implementation of the ft8_decode routine detects repeated calls
-!with nzhsym at 50 and provides decodes in that case.
+!
+! This implementation of the ft8_decode routine does not attempt to do what WSJT-X 2.6 does, 
+! which is to save the results of early decodes for use later in the cycle. In principle,
+! that is at best a performance enhancement. Why? Because when this decoder is invoked later
+! in the cycle, it knows all the incoming data all over again, and then more. Also in principle
+! the only decoding advantage possible for early decodes would be if the decoding algorithm takes
+! the shorter length of the incoming sound buffer into account.
 module ft8_decode
 
-  parameter (MAXFOX=1000)
-  character*12 c2fox(MAXFOX)
-  character*4  g2fox(MAXFOX)
-  integer nsnrfox(MAXFOX)
-  integer nfreqfox(MAXFOX)
-  integer n30fox(MAXFOX)
-  integer n30z
-  integer nfox
+   parameter (MAXFOX=1000)
+   character*12 c2fox(MAXFOX)
+   character*4  g2fox(MAXFOX)
+   integer nsnrfox(MAXFOX)
+   integer nfreqfox(MAXFOX)
+   integer n30fox(MAXFOX)
+   integer n30z
+   integer nfox
 
-  type :: ft8_decoder
-     procedure(ft8_decode_callback), pointer :: callback
+   type :: ft8_decoder
+      procedure(ft8_decode_callback), pointer :: callback
    contains
-     procedure :: decode
-  end type ft8_decoder
+      procedure :: decode
+   end type ft8_decoder
 
-  abstract interface
-     subroutine ft8_decode_callback (this,sync,snr,dt,freq,decoded,nap,qual)
-       import ft8_decoder
-       implicit none
-       class(ft8_decoder), intent(inout) :: this
-       real, intent(in) :: sync
-       integer, intent(in) :: snr
-       real, intent(in) :: dt
-       real, intent(in) :: freq
-       character(len=37), intent(in) :: decoded
-       integer, intent(in) :: nap 
-       real, intent(in) :: qual 
-     end subroutine ft8_decode_callback
-  end interface
+   abstract interface
+      subroutine ft8_decode_callback (this,sync,snr,dt,freq,decoded,nap,qual)
+         import ft8_decoder
+         implicit none
+         class(ft8_decoder), intent(inout) :: this
+         real, intent(in) :: sync
+         integer, intent(in) :: snr
+         real, intent(in) :: dt
+         real, intent(in) :: freq
+         character(len=37), intent(in) :: decoded
+         integer, intent(in) :: nap
+         real, intent(in) :: qual
+      end subroutine ft8_decode_callback
+   end interface
 
 contains
 
-  subroutine decode(this,callback,iwave,nQSOProgress,nfqso,nftx,newdat,  &
-       nutc,nfa,nfb,nzhsym,ndepth,emedelay,ncontest,nagain,lft8apon,     &
-       lapcqonly,napwid,mycall12,hiscall12,ldiskdat)
-    use iso_c_binding, only: c_bool, c_int
-    use timer_module, only: timer
+   subroutine decode(this,callback,iwave,nQSOProgress,nfqso,nftx,newdat,  &
+      nutc,nfa,nfb,nzhsym,ndepth,emedelay,ncontest,nagain,lft8apon,     &
+      lapcqonly,napwid,mycall12,hiscall12,ldiskdat)
+      use iso_c_binding, only: c_bool, c_int
+      use timer_module, only: timer
 
-    include 'ft8/ft8_params.f90'
+      include 'ft8/ft8_params.f90'
 
-    class(ft8_decoder), intent(inout) :: this
-    procedure(ft8_decode_callback) :: callback
-    parameter (MAXCAND=300,MAX_EARLY=100)
-    real*8 tsec,tseq
-    real s(NH1,NHSYM)
-    real sbase(NH1)
-    real candidate(3,MAXCAND)
-    real dd(15*12000),dd1(15*12000)
-    logical, intent(in) :: lft8apon,lapcqonly,nagain
-    logical newdat,lsubtract,ldupe,lrefinedt
-    logical*1 ldiskdat
-    logical lsubtracted(MAX_EARLY)
-    character*12 mycall12,hiscall12,call_1,call_2
-    character*4 grid4
-    integer*2 iwave(15*12000)
-    integer apsym2(58),aph10(10)
-    character datetime*13,msg37*37
-    character*37 allmessages(100)
-    character*12 ctime
-    integer allsnrs(100)
-    integer itone(NN)
-    integer itone_save(NN,MAX_EARLY)
-    real f1_save(MAX_EARLY)
-    real xdt_save(MAX_EARLY)
+      class(ft8_decoder), intent(inout) :: this
+      procedure(ft8_decode_callback) :: callback
+      parameter (MAXCAND=300,MAX_EARLY=100)
+      real*8 tsec,tseq
+      real s(NH1,NHSYM)
+      real sbase(NH1)
+      real candidate(3,MAXCAND)
+      real dd(15*12000),dd1(15*12000)
+      logical, intent(in) :: lft8apon,lapcqonly,nagain
+      logical newdat,lsubtract,ldupe,lrefinedt
+      logical*1 ldiskdat
+      logical lsubtracted(MAX_EARLY)
+      character*12 mycall12,hiscall12,call_1,call_2
+      character*4 grid4
+      integer*2 iwave(15*12000)
+      integer apsym2(58),aph10(10)
+      character datetime*13,msg37*37
+      character*37 allmessages(100)
+      character*12 ctime
+      integer allsnrs(100)
+      integer itone(NN)
+      integer itone_save(NN,MAX_EARLY)
+      real f1_save(MAX_EARLY)
+      real xdt_save(MAX_EARLY)
 
-    
-    this%callback => callback
-    write(datetime,1001) nutc        !### TEMPORARY ###
-1001 format("000000_",i6.6)
+      this%callback => callback
+      write(datetime,1001) nutc        !### TEMPORARY ###
+1001  format("000000_",i6.6)
 
-    call ft8apset(mycall12,hiscall12,ncontest,apsym2,aph10)
+      call ft8apset(mycall12,hiscall12,ncontest,apsym2,aph10)
 
-          dd=iwave
-           ndecodes=0
-           allmessages='                                     '
-           allsnrs=0
+      dd=iwave
+      ndecodes=0
+      allmessages='                                     '
+      allsnrs=0
 
-    ifa=nfa
-    ifb=nfb
-    if(nagain) then
-       ifa=nfqso-20
-       ifb=nfqso+20
-    endif
+      ifa=nfa
+      ifb=nfb
+      if(nagain) then
+         ifa=nfqso-20
+         ifb=nfqso+20
+      endif
 
 ! For now:
-! ndepth=1: 1 pass, bp  
-! ndepth=2: subtraction, 3 passes, bp+osd (no subtract refinement) 
+! ndepth=1: 1 pass, bp
+! ndepth=2: subtraction, 3 passes, bp+osd (no subtract refinement)
 ! ndepth=3: subtraction, 3 passes, bp+osd
-    npass=3
-    if(ndepth.eq.1) npass=1
-    do ipass=1,npass
-      newdat=.true.
-      syncmin=1.3
-      if(ndepth.le.2) syncmin=1.6
-      if(ipass.eq.1) then
-        lsubtract=.true.
-        ndeep=ndepth
-        if(ndepth.eq.3) ndeep=2
-      elseif(ipass.eq.2) then
-        n2=ndecodes
-        if(ndecodes.eq.0) cycle
-        lsubtract=.true.
-        ndeep=ndepth
-      elseif(ipass.eq.3) then
-        if((ndecodes-n2).eq.0) cycle
-        lsubtract=.true. 
-        ndeep=ndepth
-      endif 
-      call timer('sync8   ',0)
-      maxc=MAXCAND
-      call sync8(dd,ifa,ifb,syncmin,nfqso,maxc,s,candidate,   &
-           ncand,sbase)
-      call timer('sync8   ',1)
-      do icand=1,ncand
-        sync=candidate(3,icand)
-        f1=candidate(1,icand)
-        xdt=candidate(2,icand)
-        xbase=10.0**(0.1*(sbase(nint(f1/3.125))-40.0))
-        msg37='                                     '
-        call timer('ft8b    ',0)
-        call ft8b(dd,newdat,nQSOProgress,nfqso,nftx,ndeep,nzhsym,lft8apon,  &
-             lapcqonly,napwid,lsubtract,nagain,ncontest,iaptype,mycall12,   &
-             hiscall12,f1,xdt,xbase,apsym2,aph10,nharderrors,dmin,          &
-             nbadcrc,iappass,msg37,xsnr,itone)
-        call timer('ft8b    ',1)
-        nsnr=nint(xsnr)
-        xdt=xdt-0.5
-        hd=nharderrors+dmin
-        if(nbadcrc.eq.0) then
-           ldupe=.false.
-           do id=1,ndecodes
-              if(msg37.eq.allmessages(id)) ldupe=.true.
-           enddo
-           if(.not.ldupe) then
-              ndecodes=ndecodes+1
-              allmessages(ndecodes)=msg37
-              allsnrs(ndecodes)=nsnr
-           endif
-           if(.not.ldupe .and. associated(this%callback)) then
-              qual=1.0-(nharderrors+dmin)/60.0 ! scale qual to [0.0,1.0]
-              if(emedelay.ne.0) xdt=xdt+2.0
-              call this%callback(sync,nsnr,xdt,f1,msg37,iaptype,qual)
-            endif
+      npass=3
+      if(ndepth.eq.1) npass=1
+      do ipass=1,npass
+         newdat=.true.
+         syncmin=1.3
+         if(ndepth.le.2) syncmin=1.6
+         if(ipass.eq.1) then
+            lsubtract=.true.
+            ndeep=ndepth
+            if(ndepth.eq.3) ndeep=2
+         elseif(ipass.eq.2) then
+            n2=ndecodes
+            if(ndecodes.eq.0) cycle
+            lsubtract=.true.
+            ndeep=ndepth
+         elseif(ipass.eq.3) then
+            if((ndecodes-n2).eq.0) cycle
+            lsubtract=.true.
+            ndeep=ndepth
          endif
-      enddo  ! icand
-   enddo  ! ipass
+         call timer('sync8   ',0)
+         maxc=MAXCAND
+         call sync8(dd,ifa,ifb,syncmin,nfqso,maxc,s,candidate,   &
+            ncand,sbase)
+         call timer('sync8   ',1)
+         do icand=1,ncand
+            sync=candidate(3,icand)
+            f1=candidate(1,icand)
+            xdt=candidate(2,icand)
+            xbase=10.0**(0.1*(sbase(nint(f1/3.125))-40.0))
+            msg37='                                     '
+            call timer('ft8b    ',0)
+            call ft8b(dd,newdat,nQSOProgress,nfqso,nftx,ndeep,nzhsym,lft8apon,  &
+               lapcqonly,napwid,lsubtract,nagain,ncontest,iaptype,mycall12,   &
+               hiscall12,f1,xdt,xbase,apsym2,aph10,nharderrors,dmin,          &
+               nbadcrc,iappass,msg37,xsnr,itone)
+            call timer('ft8b    ',1)
+            nsnr=nint(xsnr)
+            xdt=xdt-0.5
+            hd=nharderrors+dmin
+            if(nbadcrc.eq.0) then
+               ldupe=.false.
+               do id=1,ndecodes
+                  if(msg37.eq.allmessages(id)) ldupe=.true.
+               enddo
+               if(.not.ldupe) then
+                  ndecodes=ndecodes+1
+                  allmessages(ndecodes)=msg37
+                  allsnrs(ndecodes)=nsnr
+               endif
+               if(.not.ldupe .and. associated(this%callback)) then
+                  qual=1.0-(nharderrors+dmin)/60.0 ! scale qual to [0.0,1.0]
+                  if(emedelay.ne.0) xdt=xdt+2.0
+                  call this%callback(sync,nsnr,xdt,f1,msg37,iaptype,qual)
+               endif
+            endif
+         enddo  ! icand
+      enddo  ! ipass
 
-   return
-end subroutine decode
+      return
+   end subroutine decode
 
 end module ft8_decode
